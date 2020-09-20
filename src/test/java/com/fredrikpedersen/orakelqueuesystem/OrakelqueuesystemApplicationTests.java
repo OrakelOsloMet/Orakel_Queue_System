c4b8267cba46b4f700599ee5a35f492d0d343026
FredrikPedersen <fredrikhp@gmail.com>
2020-09-20 21:19:02 +0200

    Created project

---
+++ src/test/java/com/fredrikpedersen/orakelqueuesystem/OrakelqueuesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrikpedersen.orakelqueuesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrakelqueuesystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
