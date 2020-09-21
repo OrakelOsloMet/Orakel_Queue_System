1401c91db5042643bc950803222ecf87158f9c2d
Fredrik Pedersen <fredrikhp@gmail.com>
2020-09-21 16:17:36 +0200

    Created Role and User repository interfaces

--- src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/repositories/RoleRepository.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/repositories/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.fredrikpedersen.orakelqueuesystem.dataAccessLayer.repositories;
 
-public interface RoleRepository {
+import com.fredrikpedersen.orakelqueuesystem.dataAccessLayer.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/repositories/UserRepository.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.fredrikpedersen.orakelqueuesystem.dataAccessLayer.repositories;
 
-public interface UserRepository {
+import com.fredrikpedersen.orakelqueuesystem.dataAccessLayer.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(final String username);
 }
