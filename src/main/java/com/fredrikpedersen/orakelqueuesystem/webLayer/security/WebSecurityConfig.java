14e13d582cbb013c1e7bb94ba4aa68b3999d48d9
FredrikPedersen <fredrikhp@gmail.com>
2020-09-23 21:19:03 +0200

    Enabled unauthorized requests to /api/queue

--- src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/security/WebSecurityConfig.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/security/WebSecurityConfig.java
@@ -55,8 +55,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/queue/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
