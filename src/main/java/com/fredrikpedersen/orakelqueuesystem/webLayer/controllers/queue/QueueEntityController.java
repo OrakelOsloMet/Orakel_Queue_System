c36d47447e7493dd914698e4c10ef7906e10b02f
Fredrik Pedersen <fredrikhp@gmail.com>
2020-10-07 14:48:46 +0200

    Added simple rate limiting to open API Endpoints

--- src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/queue/QueueEntityController.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/queue/QueueEntityController.java
@@ -3,11 +3,17 @@ package com.fredrikpedersen.orakelqueuesystem.webLayer.controllers.queue;
 import com.fredrikpedersen.orakelqueuesystem.dto.QueueEntityDTO;
 import com.fredrikpedersen.orakelqueuesystem.serviceLayer.queue.QueueEntityService;
 import com.fredrikpedersen.orakelqueuesystem.utilities.constants.URLs;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -23,21 +29,34 @@ import java.util.List;
 public class QueueEntityController {
 
     private final QueueEntityService queueEntityService;
+    private final Bucket bucket;
 
     public QueueEntityController(final QueueEntityService queueEntityService) {
         this.queueEntityService = queueEntityService;
+
+        Bandwidth limit = Bandwidth.classic(20, Refill.greedy(20, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<QueueEntityDTO> getAllQueueEntities() {
-        return queueEntityService.findALlNotDone();
+    public ResponseEntity<List<QueueEntityDTO>> getAllQueueEntities() {
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(queueEntityService.findALlNotDone());
+        }
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public QueueEntityDTO postQueueEntity(@RequestBody final QueueEntityDTO queueEntityDTO) {
-        return queueEntityService.createNew(queueEntityDTO);
+    public ResponseEntity<QueueEntityDTO> postQueueEntity(@RequestBody final QueueEntityDTO queueEntityDTO) {
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(queueEntityService.createNew(queueEntityDTO));
+        }
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @PostMapping(URLs.QUEUE_CONFIRM_DONE + "/{id}")
--- src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/queue/SubjectController.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/queue/SubjectController.java
@@ -2,11 +2,18 @@ package com.fredrikpedersen.orakelqueuesystem.webLayer.controllers.queue;
 
 import com.fredrikpedersen.orakelqueuesystem.serviceLayer.queue.SubjectService;
 import com.fredrikpedersen.orakelqueuesystem.utilities.constants.URLs;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 @CrossOrigin
@@ -15,13 +22,24 @@ import java.util.ArrayList;
 public class SubjectController {
 
     private SubjectService subjectService;
+    private final Bucket bucket;
 
     public SubjectController(final SubjectService subjectService) {
+
         this.subjectService = subjectService;
+
+        Bandwidth limit = Bandwidth.classic(20, Refill.greedy(20, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @GetMapping
-    public ArrayList<String> getSubjects() {
-        return subjectService.getSubjectList();
+    public ResponseEntity<ArrayList<String>> getSubjects() {
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(subjectService.getSubjectList());
+        }
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
