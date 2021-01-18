87d50e750bdc876e0366e1e631e774f31cb435c4
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-18 15:43:46 +0100

    Re-adding previous semester's subjects as a quick fix to the crashing application.

--- src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/models/queue/ESubject.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/dataAccessLayer/models/queue/ESubject.java
@@ -1,6 +1,13 @@
 package com.fredrikpedersen.orakelqueuesystem.dataAccessLayer.models.queue;
 
 public enum ESubject {
+    PROGRAMMING("Programmering"),
+    DISCRETE_MATHEMATICS("Diskret Matte"),
+    WEB_DEVELOPMENT("Web Utvikling"),
+    ALGORITHMS_AND_DATASTRUCTURES("Alogritmer og Datastrukturer"),
+    SOFTWARE_ENGINEERING("Systemutvikling"),
+    MACHINE_HUMAN_INTERACTION("MMI"),
+    WEB_APPLICATIONS("WebApps"),
     DATABASES("Databaser"),
     WEBPROGRAMING("Webprogrammering"),
     IOT("Internet of Things"),
