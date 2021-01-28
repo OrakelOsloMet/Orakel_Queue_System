186c88344b61cfbeba3962b4ce79cd43d863be56
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-28 20:46:51 +0100

    Created endpoint for retrieving CSV file

--- src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/resources/ResourceController.java
+++ src/main/java/com/fredrikpedersen/orakelqueuesystem/webLayer/controllers/resources/ResourceController.java
@@ -1,9 +1,12 @@
 package com.fredrikpedersen.orakelqueuesystem.webLayer.controllers.resources;
 
+import com.fredrikpedersen.orakelqueuesystem.serviceLayer.resources.QueueEntityExportService;
 import com.fredrikpedersen.orakelqueuesystem.utilities.constants.URLs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,6 +27,12 @@ import java.io.IOException;
 @RequestMapping(URLs.RESOURCES_BASE_URL)
 public class ResourceController {
 
+    private QueueEntityExportService queueEntityExportService;
+
+    public ResourceController(final QueueEntityExportService queueEntityExportService) {
+        this.queueEntityExportService = queueEntityExportService;
+    }
+
     @GetMapping(value = URLs.USER_GUIDE_URL, produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> getUserGuideAndDocumentation() throws IOException {
 
@@ -33,5 +42,16 @@ public class ResourceController {
                 .ok()
                 .contentLength(resource.contentLength())
                 .body(new InputStreamResource(resource.getInputStream()));
+    }
+
+    //TODO Remember to only let ADMINS call this endpoint
+    @GetMapping(value = URLs.QUEUE_DATA_EXPORT_URL)
+    public ResponseEntity<Resource> getExportedQueueData() {
+        final String filename = "QueueData.csv";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(queueEntityExportService.generateCsvStreamResourceFromEntities());
     }
 }
