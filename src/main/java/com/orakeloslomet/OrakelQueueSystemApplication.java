bcb4bb182c76bfbd6f0c231749519f37d37a82f1
Fredrik Pedersen <fredrikhp@gmail.com>
2022-01-19 15:59:52 +0100

    Resolves #22

--- src/main/java/com/orakeloslomet/OrakelQueueSystemApplication.java
+++ src/main/java/com/orakeloslomet/OrakelQueueSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrakelqueuesystemApplication {
+public class OrakelQueueSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrakelqueuesystemApplication.class, args);
+        SpringApplication.run(OrakelQueueSystemApplication.class, args);
     }
 
 }
